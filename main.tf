dea57207b37b96b2a0bef62d25cde7bf9018a816
Braulio Aguilar <braulio.inf@gmail.com>
2021-01-27 17:36:16 -0600

    fix: subnet vault

--- main.tf
+++ main.tf
@@ -11,54 +11,53 @@ provider "aws" {
   region = var.region
 }
 
-resource "aws_vpc" "vpc_dev" {
+resource "aws_vpc" "vpc_vault" {
   cidr_block           = "10.0.0.0/16"
   enable_dns_support   = true
   enable_dns_hostnames = true
 
   tags = {
-    Name = "vpc_dev"
+    Name        = "VPC Vault"
+    Environment = "testing"
   }
 }
 
-resource "aws_internet_gateway" "internet_gateway_dev" {
-  vpc_id = aws_vpc.vpc_dev.id
-
-  tags = {
-    Name = "ig_dev"
-  }
-}
-
-resource "aws_subnet" "subnet_dev" {
-  vpc_id     = aws_vpc.vpc_dev.id
-  cidr_block = "10.0.1.0/24"
-
+resource "aws_subnet" "subnet_vault" {
+  vpc_id            = aws_vpc.vpc_vault.id
+  cidr_block        = "10.0.1.0/24"
   availability_zone = "us-west-2a"
 
   tags = {
-    Name = "subnet_dev"
+    Name        = "Public Subnet Vault"
+    Environment = "testing"
   }
 }
 
-// Dirige todo el tráfico (0.0.0.0/0) a la puerta de enlace de Internet 
-// y asociar esta tabla de rutas con ambas subredes. 
-// Cada subred de la VPC debe estar asociada a una tabla de rutas.
-resource "aws_route_table" "public_dev" {
-  vpc_id = aws_vpc.vpc_dev.id
+resource "aws_internet_gateway" "internet_gateway_vault" {
+  vpc_id = aws_vpc.vpc_vault.id
+
+  tags = {
+    Name        = "Internet Gateway Vault"
+    Environment = "testing"
+  }
+}
+
+resource "aws_route_table" "route_table" {
+  vpc_id = aws_vpc.vpc_vault.id
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.internet_gateway_dev.id
+    gateway_id = aws_internet_gateway.internet_gateway_vault.id
   }
 }
 
-resource "aws_route_table_association" "route_table_association_dev" {
-  subnet_id      = aws_subnet.subnet_dev.id
-  route_table_id = aws_route_table.public_dev.id
+resource "aws_route_table_association" "route_table_association" {
+  subnet_id      = aws_subnet.subnet_vault.id
+  route_table_id = aws_route_table.route_table.id
 }
 
 
-data "aws_iam_policy_document" "ecs_agent" {
+data "aws_iam_policy_document" "policy" {
   statement {
     actions = ["sts:AssumeRole"]
 
@@ -69,26 +68,25 @@ data "aws_iam_policy_document" "ecs_agent" {
   }
 }
 
-resource "aws_iam_role" "ecs_agent" {
-  name               = "ecs-agent"
-  assume_role_policy = data.aws_iam_policy_document.ecs_agent.json
+resource "aws_iam_role" "role" {
+  name               = "role-vault"
+  assume_role_policy = data.aws_iam_policy_document.policy.json
 }
 
-resource "aws_iam_role_policy_attachment" "iam_role_policy_attachment_dev" {
-  role       = aws_iam_role.ecs_agent.name
+resource "aws_iam_role_policy_attachment" "policy_attachment" {
+  role       = aws_iam_role.role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceforEC2Role"
 }
 
-resource "aws_iam_instance_profile" "ecs_agent" {
-  name = "ecs-agent"
-  role = aws_iam_role.ecs_agent.name
+resource "aws_iam_instance_profile" "default" {
+  name = "profile-vault"
+  role = aws_iam_role.role.name
 }
 
-
-resource "aws_security_group" "security_group_dev" {
-  name        = "security_group_dev"
+resource "aws_security_group" "security_group_vault" {
+  name        = "security_group_vault"
   description = "Rules ssh/web security group"
-  vpc_id      = aws_vpc.vpc_dev.id
+  vpc_id      = aws_vpc.vpc_vault.id
 
   ingress {
     from_port   = 22
@@ -104,6 +102,13 @@ resource "aws_security_group" "security_group_dev" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    from_port   = 8200
+    to_port     = 8200
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 80
     to_port     = 80
@@ -111,8 +116,16 @@ resource "aws_security_group" "security_group_dev" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  egress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   tags = {
-    Name = "security_group_dev"
+    Name        = "Security Group Vault"
+    Environment = "testing"
   }
 }
 
@@ -121,11 +134,11 @@ module "consul_cluster" {
   source = "./modules/consul"
 
   ami_id               = var.consult_ami_id
-  iam_instance_profile = aws_iam_instance_profile.ecs_agent.name
-  security_group_id    = aws_security_group.security_group_dev.id
+  iam_instance_profile = aws_iam_instance_profile.default.name
+  security_group_id    = aws_security_group.security_group_vault.id
   user_data            = "#!/bin/bash\necho ECS_CLUSTER=my-cluster >> /etc/ecs/ecs.config"
 
-  vpc_zone_identifier = [aws_subnet.subnet_dev.id]
+  vpc_zone_identifier = [aws_subnet.subnet_vault.id]
 
   cluster_name = var.consul_cluster_name
 
@@ -139,11 +152,11 @@ module "vault_cluster" {
   source = "./modules/vault"
 
   ami_id               = var.vault_ami_id
-  iam_instance_profile = aws_iam_instance_profile.ecs_agent.name
-  security_group_id    = aws_security_group.security_group_dev.id
+  iam_instance_profile = aws_iam_instance_profile.default.name
+  security_group_id    = aws_security_group.security_group_vault.id
   user_data            = "#!/bin/bash\necho ECS_CLUSTER=my-cluster >> /etc/ecs/ecs.config"
 
-  vpc_zone_identifier = [aws_subnet.subnet_dev.id]
+  vpc_zone_identifier = [aws_subnet.subnet_vault.id]
 
   cluster_name = var.vault_cluster_name
 
